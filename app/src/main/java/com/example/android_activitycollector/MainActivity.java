73230f28c8a0e879809958004124ef8bdf2ca5ea
Kay You <ykmeory@163.com>
2018-04-25 18:20:27 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/android_activitycollector/MainActivity.java
+++ app/src/main/java/com/example/android_activitycollector/MainActivity.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 
 /**
  * 集中管理Activity：
- * 1、封装一个CollectorActivity用于集中管理Activity
+ * 1、封装一个ActivityCollector用于集中管理Activity
  * 2、定义BaseActivity，调用添加、删除Activity的方法
- * 3、之后无论想在哪里销毁所有Activity（或退出程序），调用CollectorActivity.finishAllActivity()即可
+ * 3、之后无论想在哪里销毁所有Activity（或退出程序），调用ActivityCollector.getActivityCollector().finishAll();即可
  */
 public class MainActivity extends BaseActivity {
 
@@ -16,7 +16,7 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         /*
-        可在调用CollectorActivity.finishAllActivity()之后，
+        可在调用ActivityCollector.getActivityCollector().finishAll();之后，
         再调用killProcess()，以保证完全退出。
 
         注意：killProcess()只能用于杀掉当前进程，不能杀死其他进程

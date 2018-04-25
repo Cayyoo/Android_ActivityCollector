a9b9f9c1d8e519552984cee307f5cccad3bd17b6
Kay You <ykmeory@163.com>
2018-04-25 18:05:59 +0800

    Update CollectorActivity.java

--- app/src/main/java/com/example/android_activitycollector/CollectorActivity.java
+++ app/src/main/java/com/example/android_activitycollector/CollectorActivity.java
@@ -2,44 +2,164 @@ package com.example.android_activitycollector;
 
 import android.app.Activity;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 /**
- * Created by Administrator on 2017/10/27.
+ * Created by Administrator on 2017/12/18.
  *
- * Activity管理器
+ * @author Administrator
+ *         Activity管理类
  */
-
-public class CollectorActivity {
-    public static List<Activity> activityList=new ArrayList<>();
-
+public class ActivityCollector {
     /**
-     * 新增一个Activity
-     * @param activity
+     * 使用java.util.Stack来保存所有已打开的Activity。
+     * 也可考虑使用双端队列java.util.Deque。
      */
-    public static void addActivity(Activity activity){
-        activityList.add(activity);
+    private static Stack<Activity> activityStack;
+    //private static List<Activity> activityStack;
+
+    private static volatile ActivityCollector instance;
+
+    private ActivityCollector() {
+        if (activityStack == null) {
+            activityStack = new Stack<>();
+        }
     }
 
     /**
-     * 删除一个Activity
-     * @param activity
+     * 单一实例
      */
-    public static void removeActivity(Activity activity){
-        activityList.remove(activity);
+    public static ActivityCollector getActivityCollector() {
+        if (null == instance) {
+            synchronized (ActivityCollector.class) {
+                if (null == instance) {
+                    instance = new ActivityCollector();
+                }
+            }
+        }
+        return instance;
     }
+
+
+    /**
+     * 添加Activity到堆栈
+     */
+    public void addActivity(Activity activity) {
+        //activityStack.add(activity);
+        activityStack.push(activity);
+    }
+
+    /**
+     * 移除Activity出堆栈
+     */
+    public void removeActivity(Activity activity) {
+        if (activityStack.contains(activity)) {
+            activityStack.remove(activity);
+        }
+    }
+
+
+    /**
+     * 获取当前Activity（堆栈中最后一个压入的）
+     */
+    public Activity getActivity() {
+        return activityStack.get(activityStack.size() - 1);
+    }
+
+    /**
+     * 获取指定类名的Activity
+     */
+    public Activity getActivity(Class<?> cls) {
+        if (activityStack != null) {
+            for (Activity activity : activityStack) {
+                if (activity.getClass().equals(cls)) {
+                    return activity;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 结束当前Activity（堆栈中最后一个压入的）
+     */
+    public void finishActivity() {
+        Activity activity = activityStack.get(activityStack.size() - 1);
+        finishActivity(activity);
+    }
+
+    /**
+     * 结束指定的Activity
+     */
+    public void finishActivity(Activity activity) {
+        if (activity != null) {
+            activityStack.remove(activity);
+            activity.finish();
+        }
+    }
+
+    /**
+     * 结束指定类名的Activity
+     */
+    public void finishActivity(Class<?> cls) {
+        for (Activity activity : activityStack) {
+            if (activity.getClass().equals(cls)) {
+                finishActivity(activity);
+                break;
+            }
+        }
+    }
+
 
     /**
      * 关闭所有Activity
      */
-    public static void finishAllActivity(){
-        for (Activity activity : activityList){
-            if (!activity.isFinishing()){
+    public static void finishAll() {
+        for (Activity activity : activityStack) {
+            if (!activity.isFinishing()) {
                 activity.finish();
             }
         }
-        activityList.clear();
+        activityStack.clear();
+    }
+
+    /**
+     * 关闭所有(前台、后台)Activity，按先进先出顺序关闭
+     */
+    public void finishAllFirstInFirstOut() {
+        int len = activityStack.size();
+        for (int i = 0; i < len; i++) {
+            Activity activity = activityStack.pop();
+            activity.finish();
+        }
+        activityStack.clear();
+    }
+
+    /**
+     * 关闭所有(前台、后台)Activity，按先进后出顺序关闭
+     */
+    public void finishAllLastInFirstOut() {
+        for (int i = activityStack.size() - 1; i >= 0; i--) {
+            Activity activity = activityStack.get(i);
+            if (!activity.isFinishing()) {
+                activity.finish();
+            }
+        }
+        activityStack.clear();
+    }
+
+
+    /**
+     * 退出应用程序
+     */
+    public void exitApp() {
+        try {
+            finishAllLastInFirstOut();
+            System.exit(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
